c0cb6e1507dee9b79fa72fc4d4f786c49c29937d
Alvaro Garcia <derecerca@gmail.com>
2017-06-11 13:35:13 +0200

    Refactor: move production code to production namespace

--- test/payment_load_balancer_spike/core_test.clj
+++ test/payment_load_balancer_spike/core_test.clj
@@ -3,25 +3,6 @@
   (:require
     [payment-load-balancer-spike.core :refer :all]))
 
-(defn
-  smallest-bucket
-  [m]
-  (let [key-and-size (reduce (fn [acc [k v]] (conj acc {:key k :length (count v)})) '() m)
-        decreasing-by-size #(> (:length %2) (:length %1))]
-    (->>
-      key-and-size
-      (sort decreasing-by-size)
-      first
-      :key)))
-
-(defn
-  process
-  [rules candidate repository]
-  (let [bucket-name ((:fn (first rules)) @repository)
-        _ (if-not (find @repository bucket-name)
-            (swap! repository assoc bucket-name []))]
-    (let [add-to-bucket (fn [bucket-name] (swap! repository update-in [bucket-name] conj candidate))]
-      (add-to-bucket bucket-name))))
 
 (defn
   generate-payments
