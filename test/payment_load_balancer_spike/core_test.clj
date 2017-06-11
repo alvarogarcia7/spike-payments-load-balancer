be26b6a17b9aad55a3a52975c8ef045a88303011
Alvaro Garcia <derecerca@gmail.com>
2017-06-11 12:13:00 +0200

    Acceptance green

--- test/payment_load_balancer_spike/core_test.clj
+++ test/payment_load_balancer_spike/core_test.clj
@@ -3,11 +3,39 @@
   (:require
     [payment-load-balancer-spike.core :refer :all]))
 
+(def
+  payments
+  (atom {:bucket1 [] :bucket2 []}))
+
+(defn
+  process
+  [candidate repository]
+  (let [next-bucket (fn [m] (:key (first (sort #(> (:length %2) (:length %1)) (reduce (fn [acc [k v]] (conj acc {:key k :length (count v)})) '() m)))))
+        new-bucket (update @repository (next-bucket @repository) conj candidate)]
+    (reset! repository new-bucket)
+    ;(println @repository)
+    ;(println (str "received " candidate))
+    ))
+
+(defn
+  add-payments
+  [number]
+  (map #(-> {:id %}) (range 100)))
+
 (facts
   "about the rules"
   (fact
-    "splits evenly by number of payments"
-    (->
-      (add-payments 100)
-      process) => (assert (and (= 50 (get @payments :bucket1))
-                               (= 50 (get @payments :bucket2))))))
+    "add 100 payments"
+    (let [payments100 (atom {:bucket2 [] :bucket1 []})
+          _ (doall
+              (->>
+                (add-payments 100)
+                (map #(process % payments100))))]
+      (fact
+        "splits evenly by number of payments"
+        (println @payments100)
+        (count (get @payments100 :bucket1)) => 50
+        (count (get @payments100 :bucket2)) => 50
+        ))))
+
+
