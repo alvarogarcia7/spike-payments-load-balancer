b94d894c6dbbe2a322e8cbbba5822794a4caf61a
Alvaro Garcia <derecerca@gmail.com>
2017-06-11 13:55:41 +0200

    Refactor: introduce domain concept (History)

--- test/payment_load_balancer_spike/core_test.clj
+++ test/payment_load_balancer_spike/core_test.clj
@@ -25,31 +25,31 @@
     "splitting evenly in two buckets"
     (fact
       "when both buckets are defined"
-      (let [repository (atom {:bucket2 [] :bucket1 []})]
-        (process-payments repository
+      (let [history (atom {:bucket2 [] :bucket1 []})]
+        (process-payments history
                           [{:fn smallest-bucket}]
                           (generate-payments 100))
-        (count (get @repository :bucket1)) => 50
-        (count (get @repository :bucket2)) => 50
+        (count (get @history :bucket1)) => 50
+        (count (get @history :bucket2)) => 50
         )
       )
     (fact
       "when one of the buckets is not defined"
-      (let [repository1 (atom {:bucket2 []})]
-        (process-payments repository1
+      (let [history (atom {:bucket2 []})]
+        (process-payments history
                           [{:fn (get rules :only-5-in-bucket2)}]
                           (generate-payments 10))
-        (count (get @repository1 :bucket1)) => 5
-        (count (get @repository1 :bucket2)) => 5
+        (count (get @history :bucket1)) => 5
+        (count (get @history :bucket2)) => 5
         ))
     (fact
       "when none of the buckets is defined"
-      (let [repository1 (atom {})]
-        (process-payments repository1
+      (let [history (atom {})]
+        (process-payments history
                           [{:fn (get rules :only-5-in-bucket2)}]
                           (generate-payments 10))
-        (count (get @repository1 :bucket1)) => 5
-        (count (get @repository1 :bucket2)) => 5
+        (count (get @history :bucket1)) => 5
+        (count (get @history :bucket2)) => 5
         ))
     ))
 
