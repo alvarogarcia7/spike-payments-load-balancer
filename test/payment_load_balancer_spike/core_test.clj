4501f27e247874ad0d551aebfc7c13ff66eefcde
Alvaro Garcia <derecerca@gmail.com>
2017-06-11 13:52:38 +0200

    Refactor: extract explaining function

--- test/payment_load_balancer_spike/core_test.clj
+++ test/payment_load_balancer_spike/core_test.clj
@@ -9,41 +9,45 @@
   [number]
   (map #(-> {:id %}) (range number)))
 
+
+(defn
+  process-payments
+  [repository rules payments]
+  (doall (map #(process rules % repository) payments)))
+
 (facts
   "about the rules"
   (fact
     "splitting evenly in two buckets"
     (fact
       "when both buckets are defined"
-      (let [repository (atom {:bucket2 [] :bucket1 []})
-            process (partial process [{:fn smallest-bucket}])
-            _ (doall
-                (->>
-                  (generate-payments 100)
-                  (map #(process % repository))))]
+      (let [repository (atom {:bucket2 [] :bucket1 []})]
+        (process-payments repository
+                          [{:fn smallest-bucket}]
+                          (generate-payments 100))
         (count (get @repository :bucket1)) => 50
-        (count (get @repository :bucket2)) => 50))
+        (count (get @repository :bucket2)) => 50
+        )
+      )
     (fact
       "when one of the buckets is not defined"
-      (let [repository1 (atom {:bucket2 []})
-            process (partial process [{:fn (fn [m] (if (>= (count (get m :bucket2)) 5) :bucket1 :bucket2))}])
-            _ (doall
-                (->>
-                  (generate-payments 10)
-                  (map #(process % repository1))))]
+      (let [repository1 (atom {:bucket2 []})]
+        (process-payments repository1
+                          [{:fn (fn [m] (if (>= (count (get m :bucket2)) 5) :bucket1 :bucket2))}]
+                          (generate-payments 10))
         (count (get @repository1 :bucket1)) => 5
-        (count (get @repository1 :bucket2)) => 5))
+        (count (get @repository1 :bucket2)) => 5
+        ))
     (fact
       "when none of the buckets is defined"
-      (let [repository1 (atom {})
-            process (partial process [{:fn (fn [m] (if (>= (count (get m :bucket2)) 5) :bucket1 :bucket2))}])
-            _ (doall
-                (->>
-                  (generate-payments 10)
-                  (map #(process % repository1))))]
+      (let [repository1 (atom {})]
+        (process-payments repository1
+                          [{:fn (fn [m] (if (>= (count (get m :bucket2)) 5) :bucket1 :bucket2))}]
+                          (generate-payments 10))
         (count (get @repository1 :bucket1)) => 5
-        (count (get @repository1 :bucket2)) => 5))
-
+        (count (get @repository1 :bucket2)) => 5
+        ))
     ))
+
 
 
